074043fb46c1cf1adc3893c2a77afd17f1c276d4
reinheimermat <mreinheimer0@gmail.com>
2022-03-09 17:00:55 -0300

    manipulando DOM

---
+++ Manipulando DOM/js/script.js
@@ -0,0 +1,14 @@
+// 1. Mudando valores com .value
+const mudarTexto = () => {
+    document.getElementById('texto1').value = "Buenas"
+}
+
+// 2. Mudando valores com .innerText
+const mudarTexto2 = () => {
+    document.getElementById('texto2').innerText = "Novo texto"
+}
+
+// 3. Mudando valores com .innerTextHTML
+const mudarTexto3 = () => {
+    document.getElementById('texto3').innerHTML = "innerHtml"
+}
