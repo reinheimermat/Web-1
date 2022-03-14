d28a45b06e474caa16deec8aa7f74e943db103eb
reinheimermat <mreinheimer0@gmail.com>
2022-03-14 16:38:27 -0300

    Manipulando DOM.

--- Manipulando DOM/js/script.js
+++ Manipulando DOM/js/script.js
@@ -3,7 +3,7 @@
 
 // 1. Mudando valores com .value
 function mudarTexto() {
-    document.querySelector('.texto1').value = "Buenas";
+    document.getElementById('texto1').value = "Buenas";
 }
 
 // 2. Mudando valores com .innerText
@@ -29,68 +29,77 @@ function mudarTexto3() {
         "</ul>";
 }
 
-// 4. Mudando uma imagem a clicar
-const mudarImagem = () => {
-    document.querySelector('#img1').src = "img/img2.jpg"
-}
-
-const voltarImagem = () => {
-    document.querySelector('#img1').src = "img/img1.jpg"
+// 4. Mudando uma imagem ao clicar
+function mudarImagem() {
+    document.getElementById('img1').src = "img/002.jpg";
 }
 
 // 5. Mudando uma imagem ao passar o mouse
-const mudarImagem2 = () => {
-    document.querySelector('#img2').src = "img/img1.jpg"
+function mudarImagem2() {
+    document.getElementById('img2').src = "img/001.jpg";
 }
 
-const voltarImagem2 = () => {
-    document.querySelector('#img2').src = "img/img2.jpg"
+function voltarImagem() {
+    document.getElementById('img2').src = "img/002.jpg";
 }
 
 // 6. Mudando o CSS (estilo)
-const mudarCorTexto = () => {
-    const texto = document.querySelector('#texto6');
+function mudarCorTexto() {
+    // Guarda o elemnto inteiro
+    const texto = document.getElementById('texto6');
 
-    texto.innerHTML = "Ok"
-    texto.style.color = 'red'
-    texto.style.backgroundColor = "yellow"
+    // texto.innerHTML = "OK";
+    // texto.style.color = "red";
+    texto.style.color = "#990055";
+    // texto.style.fontWeight = "bold";
+    texto.style.fontWeight = "700";
+    // texto.style.backgroundColor = "yellow";
+    texto.style.padding = "10px";
+
+    // Adiciona um a classe CSS no elemento HTML
+    texto.classList.add("box");
 }
 
-const voltarCorTexto = () => {
-    const texto = document.querySelector('#texto6');
-    texto.style.color = 'black'
-}
-
-// 7. Mudando o CSS (com parâmetro)
-const mudarTexto7 = (elemento) => {
-    elemento.innerHTML = "texto alterado pelo javascript";
+// 7. Mudando o CSS (com parâmetros)
+function mudarTexto7(elemento) {
+    elemento.innerHTML = "Texto alterado pelo JavaScript";
 }
 
 // 8. Chamando uma function que chama outra
-const calcular = (valor1, valor2) => {
-    valor1 = parseFloat(valor1.value)
-    valor2 = parseFloat(valor2.value)
+function calcular(valor1, valor2) {,,,,,,,
+    // Converte para número
+    /*  const valorConvertido1 = parseInt(valor1.value);
+        const valorConvertido2 = parseInt(valor2.value); */
 
-    const resultado = somar(valor1, valor2)
+    valor1 = parseFloat(valor1.value);
+    valor2 = parseFloat(valor2.value);
 
-    mostrarResultado(resultado)
+    /*  const resultado = valorConvertido1 + valorConvertido2;
+        const resultado = valor1 + valor2; */
+
+    const resultado = somar(valor1, valor2);
+
+    // Mostra o resultado
+    mostrarResultado(resultado);
 }
 
-const somar = (valor1, valor2) => {
-    return valor1 + valor2
+function somar(valor1, valor2) {
+    return valor1 + valor2;
 }
 
-const mostrarResultado = (resultado) => {
-    document.querySelector('#resultado').innerHTML = resultado
+function mostrarResultado(resultado) {
+    document.getElementById('resultado').innerHTML = resultado;
 }
 
 // 9. Clicar, segurar e soltar
-const segurar = (elemento) => {
-    elemento.innerHTML = 'Solta!'
-    elemento.style.backgroundColor = "#252977"
+function segurar(elemento) {
+    elemento.innerHTML = "Solte!";
+    elemento.style.color = "#f9f9f9";
+    elemento.style.backgroundColor = "#252977";
 }
 
-const soltar = (elemento) => {
-    elemento.innerHTML = "clica!"
-    elemento.style.backgroundColor = "#ff7f50"
+soltar = (elemento) => {
+    elemento.innerHTML = "Clicar!";
+    elemento.style.color = "#232323";
+    elemento.style.backgroundColor = "#ff7f50";
 }
