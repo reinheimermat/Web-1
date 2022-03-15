1e1d57a8e43cddab24707786501892f5d1e8a65d
reinheimermat <mreinheimer0@gmail.com>
2022-03-14 22:04:32 -0300

    commit

--- JavaScript básico/js/script.js
+++ JavaScript básico/js/script.js
@@ -1,3 +1,13 @@
-const somar = (n1, n2) => {
-    const res = n1 + n2
+somar = (n1, n2) => {
+    n1 = parseFloat(n1.value)
+    n2 = parseFloat(n2.value)
+    const calc = n1 + n2
+    document.querySelector('#res').innerHTML = calc
+
+}
+
+limparCampos = () => {
+    document.querySelector('#n1').value = ""
+    document.querySelector('#n2').value = ""
+    document.querySelector('#res').innerHTML = ""
 }
